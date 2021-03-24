65c1921ca6afc5fbe167300032ef88bff42d0960
lamiyarahman92 <lamiyarahman92@gmail.com>
2021-03-24 10:55:41 +0600

    test

---
+++ Lab2/Submitted.php
@@ -0,0 +1,15 @@
+<html>
+      <body>
+	        <h1>Form Submitted</h1>
+			
+			<?php
+			    echo "Name:" .$_REQUEST["name"]."<br>";
+				echo "Password:".$_REQUEST["pass"]."<br>";
+				echo "Gender:".$_REQUEST["Gender"]."<br>";
+				echo "Profession:".$_REQUEST["Profession"]."<br>";
+				echo "Bio:".$_REQUEST["Bio"]."<br>";
+			     
+				 
+			?>
+	  </body>
+</html>
